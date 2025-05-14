98f4b8a7175b15f5bcf3b486c80113b458e03acb
Jaydeep Chauhan <jchauhan@scan-it.com.sg>
2025-05-14 20:01:22 +0530

    MySQL and Hibernate Config

--- note-application/src/main/java/com/example/notes/NoteApplication.java
+++ note-application/src/main/java/com/example/notes/NoteApplication.java
@@ -2,9 +2,8 @@ package com.example.notes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class NoteApplication {
 
 	public static void main(String[] args) {
